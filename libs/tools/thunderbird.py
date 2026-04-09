37af6fff81e9ddcb55d227816172e4b4c9f41d43
eric-burel <eb@lbke.fr>
2026-04-09 21:37:31 +0200

    proper handling of attachment in thunderbird draft tool

--- libs/tools/thunderbird.py
+++ libs/tools/thunderbird.py
@@ -1,17 +1,41 @@
+from dataclasses import dataclass
 import email
 from os import path
+import re
 import subprocess
-from typing import Optional, TypedDict
+from typing import Optional
 
 from langchain.tools import tool
 
 
-class EmailContent(TypedDict):
+# dataclass is the best way to get schema + custom methods
+# (Pydantic would require clumsy JSON syntax ; TypedDict doesn't support methods)
+@dataclass
+class EmailContent:
     subject: str
     to: str
     body: str
     """ Must be an absolute path"""
-    attachment: Optional[str]
+    attachment: Optional[str] = None  # default value matters even when using Optional with dataclass
+
+    def generate_email(self):
+        to = getattr(self, "to", "")
+        safe_to = f"~~{to}~~" if to else ""
+        mail_content = "subject='{subject}',to='{to}',body='{body}'".format(
+            subject=getattr(self, "subject", ""),
+            to=safe_to,
+            body=getattr(self, "body", ""),
+        )
+        attachment = getattr(self, "attachment", "")
+        if attachment:
+            if not path.exists(attachment):
+                return f"Attachment file {attachment} doesn't exist"
+            if not path.isabs(attachment):
+                print("WARNING:",
+                      f"Attachment file path must be absolute, got {attachment}")
+                attachment = path.abspath(attachment)
+            mail_content += f",attachment='{attachment}'"
+        return mail_content
 
 
 @tool(
@@ -25,28 +49,11 @@ def compose_draft_email_thunderbird(emailContent: EmailContent):
 
     """
     # https://kb.mozillazine.org/Command_line_arguments_-_Thunderbird
-    attachment = emailContent.get("attachment", "")
-    if attachment:
-        if not path.exists(attachment):
-            return f"Attachement file {attachment} doesn't exist"
-        if not path.isabs(attachment):
-            print("WARNING:",
-                  f"Attachment file path must be absolute, got {attachment}")
-            attachment = path.abspath(attachment)
     # For safety reasons, "to" is voluntarily made wrong
     # so we reduce the risk of an accidental send / piracy
-    to = emailContent.get("to", "")
-    safe_to = f"~~{to}~~" if to else ""
-    mail_content = "subject='{subject}',to='{to}',body='{body}',attachment='{attachment}'".format(
-        subject=emailContent.get("subject", ""),
-        to=safe_to,
-        body=emailContent.get("body", ""),
-        attachment=attachment
-    )
-    print(mail_content)
+    mail_content = emailContent.generate_email()
     res = subprocess.run(
         ["thunderbird", "-compose", mail_content])
-    print(mail_content)
     return res.returncode
 
 
--- tests/unit/tools/test_thunderbird.py
+++ tests/unit/tools/test_thunderbird.py
@@ -1,6 +1,6 @@
 import unittest
 
-from libs.tools.thunderbird import compose_draft_email_thunderbird
+from libs.tools.thunderbird import EmailContent, compose_draft_email_thunderbird, EmailContent
 
 
 class TestThunderbirdTools(unittest.TestCase):
@@ -8,3 +8,16 @@ class TestThunderbirdTools(unittest.TestCase):
     def test_draft(self):
         res = compose_draft_email_thunderbird.func({"subject": "hello"})
         self.assertEqual(res, 0)
+
+    def test_no_attachement(self):
+        email_content = EmailContent(
+            subject="hello", to="test@test.test", body="world")
+        res = email_content.generate_email()
+        self.assertFalse("attachment" in res)
+
+    def test_attachement(self):
+        email_content = EmailContent(
+            subject="hello", to="test@test.test", body="world", attachment=__file__)
+        res = email_content.generate_email()
+        print(res)
+        self.assertTrue(f"attachment='{__file__}'" in res)
