b62a4caf4fe7d858762b9f65f06dcbe0a6a256be
eric-burel <eb@lbke.fr>
2026-06-03 08:11:29 +0200

    allow AGENTS.md to be located in .agents

--- libs/contexteng/agents_md_resolver.py
+++ libs/contexteng/agents_md_resolver.py
@@ -9,8 +9,9 @@ from libs.contexteng.folder_or_file_resolver import validate_working_directory
 
 def resolve_agent_md(working_directory: str) -> tuple[str, str] | tuple[None, None]:
     """
-        Find the closest AGENTS.md file starting from working directory
-        (where fais was called from terminal)
+        Find the closest AGENTS.md file starting from working directory down to home folder
+        Working directory = where fais was called from terminal
+        AGENTS.md file can be nested in a ".agents" subfolder too.
         - Check if above home folder, early fail otherwise
         - Look for AGENTS.md file in current folder, then below etc. until home folder
         - If found, return its content, else return None
@@ -26,10 +27,15 @@ def resolve_agent_md(working_directory: str) -> tuple[str, str] | tuple[None, No
     folder = wd_abs_path
     while True:
         agents_md_path = os.path.join(folder, "AGENTS.md")
+        nested_agents_md_path = os.path.join(folder, ".agents", "AGENTS.md")
         if os.path.isfile(agents_md_path):
             with open(agents_md_path, "r") as f:
                 content = f.read()
             return agents_md_path, content
+        if os.path.isfile(nested_agents_md_path):
+            with open(nested_agents_md_path, "r") as f:
+                content = f.read()
+            return nested_agents_md_path, content
         if folder == home_folder or len(folder) < len(home_folder):
             break
         # dirname of a dir is its parent if there is no final slash
